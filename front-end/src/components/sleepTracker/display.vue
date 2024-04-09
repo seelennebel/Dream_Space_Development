db74aa0637c3bfd78bfb01a114d2c751d8737219
AlanChauque <alanchauque.ac@gmail.com>
2024-04-09 19:15:52 +0200

    Commiting sleep tracker changes

--- front-end/src/components/sleepTracker/display.vue
+++ front-end/src/components/sleepTracker/display.vue
@@ -4,7 +4,7 @@ import { ref } from 'vue';
 export default {
   setup() {
     const wakeUpTime = ref('');
-    const chosenHour = ref('');
+    const chosenHour = ref('')
 
     const displayChosenHour = () => {
       chosenHour.value = wakeUpTime.value;
